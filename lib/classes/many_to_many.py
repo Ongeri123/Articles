1cc6aba377d954d47be75c4e9662db0d99f9a573
Ongeri123 <ongerinewton10@gmail.com>
2025-08-17 01:31:27 +0300

    added methods to the classes

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,12 +1,33 @@
 class Article:
+    all = []
     def __init__(self, author, magazine, title):
+        
+        if not isinstance (title, str):
+            raise TypeError("Title must be a string")
+        if not (5 <=  len(title)<=50):
+            raise ValueError("Title must be between 5 and 50 characters")
+        
         self.author = author
         self.magazine = magazine
-        self.title = title
+        self._title = title
+        
+
+    @property
+    def title(self):
+        return self._title
+  
         
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str):
+            raise TypeError('Name should be a string')
+        if len(name) == 0:
+               raise TypeError('Name must be longer than 0 characters')
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
 
     def articles(self):
         pass
@@ -22,8 +43,35 @@ class Author:
 
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        if not isinstance(name,str):
+            raise TypeError('Names must be string')
+        if not(2<= len(name) <=16):
+            raise ValueError('Names must be between 2 and 16 characters')
+        if not isinstance(category,str):
+            raise TypeError('category must be a string')
+        if len(category)==0:
+            raise ValueError('category must be longer than 0 character')
+
+        self._name = name
+        self._category = category
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 2 <= len(value) <= 16:
+            self._name = value
+    
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
 
     def articles(self):
         pass
